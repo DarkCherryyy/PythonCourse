8419141620a103a505d359dd9699e9dda8e77282
DarkCherryyy <160158552+DarkCherryyy@users.noreply.github.com>
2024-10-27 15:57:16 +0300

    Add files via upload

---
+++ Лабораторная работа №2/task1.py
@@ -0,0 +1,17 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+month = 0
+flag = True
+
+while flag:
+    if (spend <= money_capital + salary):
+        month += 1
+    else:
+        flag = False
+    money_capital -= spend - salary
+    spend *= 1 + increase
+
+print("Количество месяцев, которое можно протянуть без долгов:", month)
